5e3def546eecc044325fba7639a5904339bcddb8
Jonathan Saina <sainajonathan@gmail.com>
2024-05-07 03:05:10 -0700

    1st commit-1

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,12 +1,10 @@
 #!/usr/bin/env ruby
 # Check if there's exactly one argument provided
 if ARGV.length == 1
-  # Regular expression to match "School"
-  regex = /School/
-  
-  # Match the argument against the regular expression
-  puts ARGV[0].match?(regex) ? ARGV[0] : ""
+  # Output all occurrences of "Holberton" in the input
+  puts ARGV[0].scan(/School/).join
 else
   puts "Usage: #{$PROGRAM_NAME} <string>"
   exit 1
 end
+
