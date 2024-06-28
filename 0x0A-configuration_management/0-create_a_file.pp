2eaa7d8e853545970d352dd23e46f8d6d6a6e2d7
Jonathan Saina <sainajonathan@gmail.com>
2024-06-28 13:04:34 -0700

    Configuration management commit 5

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# This makes a file in /tmp
+#This creates a file in /tmp
 
 file { '/tmp/school':
-  content =>'Puppet is awesome',
+  content =>'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
