b0a16fbeb07be1675ad4b7e08d5cf5756c4f42ae
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-08-13 13:27:27 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-""" Module queries the Reddit API and prints the
-    titles of the first 10 hot posts
-"""
+'''
+    this module contains the function top_ten
+'''
+import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """ Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    # Import the requests library to handle HTTP requests
-    import requests
 
-    # Send a GET request to Reddit's API to fetch the top 10
-    # hot posts from the specified subreddit
-    sub_info = requests.get(
-        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
-        headers={"User-Agent": "My-User-Agent"},  # Set a custom User-Agent
-        allow_redirects=False  # Prevent automatic redirection of the request
-    )
-
-    # Check if the request resulted in a redirect or error (status code >= 300)
-    if sub_info.status_code >= 300:
-        print('None')  # Print 'None' if there's an error or redirect
-    else:
-        # Parse the JSON response and print the titles of the top 10 hot posts
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+if __name__ == "__main__":
+    top_ten(argv[1])
