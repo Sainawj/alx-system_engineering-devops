08fd5d9d29252f36c5c1a4a6a29c73c2ba7dbc7c
Jonathan Saina <51122713+Sainawj@users.noreply.github.com>
2024-08-13 14:02:11 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -28,7 +28,7 @@ def count_words(subreddit, word_list, after=None, count_dict=None):
         url += f"&after={after}"
 
     try:
-        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, allow_redirects=False)
+        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}, allow_redirects=False)
         if response.status_code != 200:
             print(f"Failed to fetch data: Status code {response.status_code}")
             return
